95ebe78bdf9e652585b8fd4f1cc350d35a1de68d
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-07-22 09:01:55 -0500

    Working on Spring Framework Configuration

---
+++ Section_5_Spring_Framework_Configuration/sfg-di-life-cycle-demo/src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+/**
+ * Created by jt on 12/27/19.
+ */
+
+public class I18nEnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
