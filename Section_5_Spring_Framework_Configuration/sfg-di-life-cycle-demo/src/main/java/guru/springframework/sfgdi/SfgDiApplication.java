5b3d30e9e4aedc99c8874ddc09e53b80e40817f8
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-07-22 10:09:43 -0500

    Working on Spring Configs

--- Section_5_Spring_Framework_Configuration/sfg-di-life-cycle-demo/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ Section_5_Spring_Framework_Configuration/sfg-di-life-cycle-demo/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,9 +4,8 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"guru.springframework.sfgdi","package com.springframework.pets"})
+
 @SpringBootApplication
 public class SfgDiApplication {
 
--- Section_5_Spring_Framework_Configuration/sfg-di-life-cycle-demo/src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ Section_5_Spring_Framework_Configuration/sfg-di-life-cycle-demo/src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,32 @@
 package guru.springframework.sfgdi.config;
 
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog","default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+      return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
 
 
     @Profile({"ES", "default"})
@@ -17,10 +36,14 @@ public class GreetingServiceConfig {
     }
 
 
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
 
@@ -29,12 +52,6 @@ public class GreetingServiceConfig {
     PrimaryGreetingService primaryGreetingService(){
         return new PrimaryGreetingService();
     }
-
-    @Bean
-    ConstructorGreetingService constructorGreetingService(){
-        return new ConstructorGreetingService();
-    }
-
 
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService(){
