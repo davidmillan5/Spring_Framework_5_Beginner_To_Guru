7a7dd8b9e7581fb09f09821b0bfaea0abfe082c6
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-07-28 21:30:14 -0500

    Working on Spring MVC Project

---
+++ Section_7_Web_Development_With_Spring_MVC/spring5-recipe-app/src/test/java/com/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springframework.spring5recipeapp;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
